df0d57d23cb6f5ca9b3ccbb7583bd0cdf37d4c85
eduairet <hola@eduairet.com>
2023-01-09 21:50:22 -0600

    First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+// Data
+import { PANGRAMS } from '../content/db-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = { title: 'EATangular' };
+
+  pangrams = [...PANGRAMS];
+
+  onKeyUp(str: string): void {
+    this.data.title = str;
+  }
 }
---
+++ src/content/db-data.ts
@@ -0,0 +1,6 @@
+export const PANGRAMS: Array<string> = [
+  'Relaxing in basins at the end of inlets terminates the endless tests from the box.',
+  'Carved symbols in a mountain hollow on the bank of an inlet irritated an eccentric person.',
+  'Cross valley and plain to steal coins from Saudi mint.',
+  'An Abyssinian fly playing a Celtic violin was annoyed by trashy flags on which were the Hebrew letter qoph.',
+];
